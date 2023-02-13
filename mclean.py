b6221000a5de21d82bbb8834ba7eba298ae5ad5c
solavrov <2slavrov@gmail.com>
2023-02-13 11:54:02 +0300

    correct closing for multi pc

--- mclean.py
+++ mclean.py
@@ -1,9 +1,9 @@
 import sqlite3
 from db_funs_mult import clean_block_done
 
-con = sqlite3.connect("Z:\\minja\\minja.db")
-print("Cleaning minja.db...")
+con = sqlite3.connect("Z:\\minja\\minja2.db")
+print("Cleaning minja2.db...")
 clean_block_done(con)
 print("Cleaned!")
-input("Press any key to close window")
+input("Press any key to close window\n")
 con.close()
--- minja2.py
+++ minja2.py
@@ -14,6 +14,7 @@ def task(minja_number):
     target_len = len(target)
     block_size = 10 ** 5
     prob_to_fail = 1 - 1 / 58 ** 6
+    closing = False
     print("Hello from Minja #" + str(minja_number) + "! Working...")
     while True:
         s = get_status(con, fair=0)
@@ -25,15 +26,19 @@ def task(minja_number):
             if k[0:target_len] == target:
                 print(k)
                 add_keys(con, k, sk_hex)
-            if keyboard.is_pressed('windows+F12'):
-                con.close()
-                return 0
+            if keyboard.is_pressed('windows+F12') and not closing:
+                print("Minja #" + str(minja_number) + " is closing...")
+                closing = True
         mark_block_done(con, s.block_to_do)
         cut_block_done(con)
         found = count_found(con)
         print(str(s.block_to_do) + " done by Minja #" + str(minja_number) +
               ", found: " + str(found - found0) +
               ", accrued prob: " + str(roundac(100 * (1 - prob_to_fail ** (s.block_to_do * block_size)))) + '%')
+        if closing:
+            con.close()
+            print("Minja #" + str(minja_number) + " is closed!")
+            return 0
 
 
 if __name__ == '__main__':
@@ -46,9 +51,10 @@ if __name__ == '__main__':
         time.sleep(40/n)
     for p in processes:
         p.join()
-    con_clean = sqlite3.connect("Z:\\minja\\minja2.db")
-    print("Cleaning minja2.db...")
-    clean_block_done(con_clean)
-    con_clean.close()
-    print("Cleaned!")
-    input("Press any key to close window")
+    # con_clean = sqlite3.connect("Z:\\minja\\minja2.db")
+    # print("Cleaning minja2.db...")
+    # clean_block_done(con_clean)
+    # con_clean.close()
+    # print("Cleaned!")
+    print("All is finished correctly!")
+    input("Press any key to close window\n")
