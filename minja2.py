f60381c8930a157be6980b85d0faee785a879b05
solavrov <2slavrov@gmail.com>
2023-02-13 14:52:54 +0300

    back to individual counter

--- minja2.py
+++ minja2.py
@@ -2,18 +2,17 @@ import sqlite3
 import keyboard
 import time
 import multiprocessing
-from db_funs_mult import get_status, add_block_taken, mark_block_done, add_keys, \
-    clean_block_done, count_found, cut_block_done
+from db_funs_mult import get_status, add_block_taken, mark_block_done, add_keys, cut_block_done
 from funs import get_address_from_sk_hex, roundac
 
 
 def task(minja_number):
     con = sqlite3.connect("Z:\\minja\\minja2.db")
-    found0 = count_found(con)
     target = '1NiNja'
     target_len = len(target)
     block_size = 10 ** 5
     prob_to_fail = 1 - 1 / 58 ** 6
+    counter = 0
     closing = False
     print("Hello from Minja #" + str(minja_number) + "! Working...")
     while True:
@@ -26,15 +25,15 @@ def task(minja_number):
             if k[0:target_len] == target:
                 print(k)
                 add_keys(con, k, sk_hex)
+                counter += 1
             if keyboard.is_pressed('windows+F12') and not closing:
                 print("Minja #" + str(minja_number) + " is closing...")
                 closing = True
         mark_block_done(con, s.block_to_do)
         cut_block_done(con)
-        found = count_found(con)
         print(str(s.block_to_do) + " done by Minja #" + str(minja_number) +
-              ", found: " + str(found - found0) +
-              ", accrued prob: " + str(roundac(100 * (1 - prob_to_fail ** (s.block_to_do * block_size)))) + '%')
+              ", found: " + str(counter) +
+              ", prob: " + str(roundac(100 * (1 - prob_to_fail ** (s.block_to_do * block_size)))) + '%')
         if closing:
             con.close()
             print("Minja #" + str(minja_number) + " is closed!")
@@ -51,10 +50,5 @@ if __name__ == '__main__':
         time.sleep(40/n)
     for p in processes:
         p.join()
-    # con_clean = sqlite3.connect("Z:\\minja\\minja2.db")
-    # print("Cleaning minja2.db...")
-    # clean_block_done(con_clean)
-    # con_clean.close()
-    # print("Cleaned!")
     print("All is finished correctly!")
     input("Press any key to close window\n")
